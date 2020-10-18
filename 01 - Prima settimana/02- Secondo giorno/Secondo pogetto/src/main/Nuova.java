2937fc4261e5ab12652c166c0c13c78ffa1325c6
iltommi1995 <tomasdanielavilavisintin@yahoo.it>
2020-10-18 20:41:04 +0200

    appunti

---
+++ 01 - Prima settimana/02- Secondo giorno/Secondo pogetto/src/main/Nuova.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Nuova 
+{
+	public static void main(String[] args) 
+	{
+		String esempioStringa = "Ciao";
+		int esempioInt = 3;
+		double esempioDouble = 2.3;
+		char esempioChar = "a";
+	}
+}
